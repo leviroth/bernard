eb3c783ca5c8c786739d124d55a846137b440be4
Levi Roth <Levi Roth>
2016-02-27 17:43:53 -0800

    ready for rudimentary deployment

--- server/display.py
+++ server/display.py
@@ -10,8 +10,16 @@ cur = sql.cursor()
 
 class index:
     def GET(self):
-        cur.execute('SELECT * FROM actions ORDER BY ROWID DESC')
-        return str(cur.fetchall())
+        cur.execute('SELECT time, mod, action, reason FROM actions ORDER BY ROWID DESC')
+        body = "<html><head><title>foo</title></head><body><table>"
+        for row in cur.fetchall():
+            body += "<tr>"
+            for col in row:
+                body += "<td>" + str(col) + "</td>"
+            body += "</tr>"
+        body += "</table></body></html>"
+
+        return body
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
