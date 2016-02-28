f2c8d72045bedb08dfad067984da7b1ba6c24919
Levi Roth <Levi Roth>
2016-02-27 16:49:41 -0800

    Very basic display

--- server/display.py
+++ server/display.py
@@ -1,12 +1,17 @@
 import web
+import sqlite3
 
 urls = (
         '/', 'index'
        )
 
+sql = sqlite3.connect('sql.db')
+cur = sql.cursor()
+
 class index:
     def GET(self):
-        return "Hello, world!"
+        cur.execute('SELECT * FROM actions')
+        return str(cur.fetchall())
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
