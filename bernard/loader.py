54da28b145aa34388101d3eed7e2324d7875fd15
Levi Roth <levimroth@gmail.com>
2016-11-14 15:16:11 -0500

    Loading rules from subreddit rules

--- bernard/loader.py
+++ bernard/loader.py
@@ -3,11 +3,21 @@ import re
 import actors
 import sqlite3
 import praw
+import logging
+import time
 
 
-class Loader:
-    def __init__(self):
-        self.db = sqlite3.connect(':memory:')
+def build_regex(triggers):
+    return re.compile("^(" + "|".join(re.escape(str(trigger))
+                                      for trigger in triggers)
+                      + ")$",
+                      re.IGNORECASE)
+
+
+class YAMLLoader:
+    def __init__(self, db, subreddit):
+        self.db = db
+        self.subreddit = subreddit
 
     def load(self, filename):
         with open(filename) as f:
@@ -23,7 +33,7 @@ class Loader:
                 for actor_config in actor_configs]
 
     def parse_rule_config(self, rule_config):
-        return Rule(rule_config['trigger'],
+        return Rule(build_regex(rule_config['trigger']),
                     [self._object_map(x) for x in rule_config['objects']],
                     rule_config['remove'],
                     self.parse_actor_config(rule_config['actions'])
@@ -41,32 +51,95 @@ class Loader:
             return praw.objects.Comment
 
 
+class SubredditRuleLoader:
+    def __init__(self, db, subreddit):
+        self.db = db
+        self.subreddit = subreddit
+
+    def load(self):
+        subrules = [rule for rule in self.subreddit.get_rules()['rules']
+                    if rule['kind'] == 'link' or rule['kind'] == 'all']
+        our_rules = []
+
+        for i, subrule in enumerate(subrules, 1):
+            note_text = "**{short_name}**\n\n{desc}".format(
+                short_name=subrule['short_name'], desc=subrule['description'])
+            our_rules.append(
+                Rule(build_regex(["RULE {n}".format(n=i),
+                                  "{n}".format(n=i),
+                                  subrule['short_name']]),
+                     [praw.objects.Submission],
+                     True,
+                     [actors.Notifier(note_text=note_text,
+                                      subreddit=self.subreddit, db=self.db)]
+                     ))
+
+        return our_rules
+
+
 class Rule:
-    def __init__(self, triggers, targets, remove, actors):
-        self.trigger = re.compile(
-            "^(" + '|'.join(re.escape(trigger) for trigger in triggers) + ")$",
-            re.IGNORECASE
-        )
+    def __init__(self, trigger, targets, remove, actors):
+        self.trigger = trigger
         self.targets = targets
         self.remove = remove
         self.actors = actors
 
+    def match(self, command, post):
+        return self.trigger.match(command) \
+            and any(isinstance(post, target) for target in self.targets)
+
     def parse(self, command, post, mod):
-        if (re.match(command)):
+        if self.match(command, post):
             for actor in self.actors:
                 actor.action(post, mod)
 
-        if self.remove:
-            post.remove()
-        else:
-            post.approve()
+            if self.remove:
+                post.remove()
+            else:
+                post.approve()
 
-        self.log_action(post, mod)
+            self.log_action(post, mod)
 
     def after(self):
         for actor in self.actors:
             actor.after()
 
+
+# Idea: Have different "command source" classes to pull commands from
+# reports, Slack, etc.
+class Browser:
+    def __init__(self, rules, subreddit):
+        self.rules = rules
+        self.subreddit = subreddit
+
+    def check_command(self, command, mod, post):
+        for rule in self.rules:
+            rule.parse(command, mod, post)
+
+    def scan_reports(self):
+        try:
+            for post in self.subreddit.get_reports(limit=None):
+                for mod_report in post.mod_reports:
+                    yield (mod_report[0], mod_report[1], post)
+        except Exception as e:
+            logging.error("Error fetching reports: {err}".format(err=e))
+
+    def run(self):
+        while True:
+            try:
+                for command in self.scan_reports():
+                    self.check_command(*command)
+                for rule in self.rules:
+                    rule.after()
+                time.sleep(30)
+            except KeyboardInterrupt:
+                print("Stopped by keyboard")
+                break
+
 if __name__ == '__main__':
-    l = Loader()
-    a = l.load('config.yaml')
+    # l = YAMLLoader(sqlite3.connect(':memory:'), 2)
+    # a = l.load('config.yaml')
+    r = praw.Reddit('bjo test')
+    s = r.get_subreddit('philosophy')
+    l = SubredditRuleLoader(sqlite3.connect(':memory:'), s)
+    rules = l.load()
