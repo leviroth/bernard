23c8790dd8d3f4eb76bdb9659787bdc8f8243676
Levi Roth <levimroth@gmail.com>
2017-01-12 17:15:58 -0500

    Add tests for Browser

---
+++ test/test_browser.py
@@ -0,0 +1,30 @@
+import praw
+import re
+import unittest.mock
+from .helper import BJOTest
+from bernard import browser, actors
+
+
+class TestBrowser(BJOTest):
+    def setUp(self):
+        super().setUp()
+        self.subactor = unittest.mock.MagicMock()
+        self.actor = actors.Actor(
+            trigger=re.compile('foo', re.I),
+            targets=[praw.models.Submission],
+            remove=False,
+            subactors=[self.subactor],
+            action_name="Remove",
+            action_details=None,
+            db=self.db,
+            cursor=self.cur,
+            subreddit=self.subreddit
+        )
+        self.browser = browser.Browser([self.actor], self.subreddit, self.db,
+                                       self.cur)
+
+    @unittest.mock.patch('time.sleep', return_value=None)
+    def test_run(self, _):
+        with self.recorder.use_cassette('TestBrowser.test_run'):
+            self.browser.run()
+            self.assertTrue(self.subactor.action.called)
