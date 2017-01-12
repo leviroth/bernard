430ab6303ea69603ef2ba2cc22dae354106f8f12
Levi Roth <levimroth@gmail.com>
2017-01-12 13:37:29 -0500

    Add tests for Actor.parse, Actor.remove_thing, Actor.log_action

--- test/test_actors.py
+++ test/test_actors.py
@@ -33,6 +33,22 @@ class TestActor(BJOTest):
             self.assertFalse(self.actor.match('foo', post))
 
 
+    @patch('time.sleep', return_value=None)
+    def test_parse(self, _):
+        post = self.r.submission(id='5e7w80')
+        with self.recorder.use_cassette('TestActor.test_parse'):
+            self.actor.parse('foo', 'TGB', post)
+            post = self.r.submission(id='5e7w80')
+            self.assertIsNotNone(post.banned_by)
+            self.assertTrue(post.locked)
+            post_id = int('5e7w80', base=36)
+            self.cur.execute('SELECT action_summary, id FROM actions '
+                             'WHERE target_type = 3 AND target_id = ?',
+                             (post_id,))
+            summary, action_id = self.cur.fetchone()
+            self.assertEqual('Remove', summary)
+            self.cur.execute('SELECT * FROM removals WHERE action_id = ?', (action_id,))
+
 class TestBanner(BJOTest):
     @patch('time.sleep', return_value=None)
     def test_action(self, _):
