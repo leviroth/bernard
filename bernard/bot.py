e86fc9aa477353d2189a5c5543156590ded8f334
Levi Roth <levimroth@gmail.com>
2017-02-27 19:43:19 -0500

    Add user agent to bot.py; provide safer loading of PRAW config

--- bernard/bot.py
+++ bernard/bot.py
@@ -10,9 +10,15 @@ import time
 _, login_conf, subs_conf, db_file = sys.argv
 
 with open(login_conf) as f:
-    config = json.load(f)
+    config_file_data = json.load(f)
 
-r = praw.Reddit(**config)
+reddit_config = {key: config_file_data[key]
+                 for key in ["client_id", "client_secret", "username",
+                             "password"]}
+reddit_config["user_agent"] = \
+  "python:/r/Philosophy reporter:v2.1 (by levimroth@gmail.com)"
+
+r = praw.Reddit(**reddit_config)
 db = sqlite3.connect(db_file)
 cursor = db.cursor()
 
