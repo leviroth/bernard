cdb6380c121f7e2e037b63d07f8bea27335994a9
Levi Roth <levimroth@gmail.com>
2018-04-08 13:22:37 -0400

    Add docstrings to discord_notifier.py

--- bernard/discord_notifier.py
+++ bernard/discord_notifier.py
@@ -1,13 +1,18 @@
+"""A logging handler that emits to a Discord webhook."""
 import requests
 from logging import Handler
 
 
 class DiscordHandler(Handler):
+    """A logging handler that emits to a Discord webhook."""
+
     def __init__(self, webhook, *args, **kwargs):
+        """Initialize the DiscordHandler class."""
         super().__init__(*args, **kwargs)
         self.webhook = webhook
 
     def emit(self, record):
+        """Emit record to the Discord webhook."""
         json = {'content': self.format(record)}
         try:
             requests.post(self.webhook, json=json)
